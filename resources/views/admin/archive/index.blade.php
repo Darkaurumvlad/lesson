23eaf956fb00976a763ff21ce5b50d30cc52a118
Darkaurumvlad <darkaurumvlad@gmail.com>
2022-02-14 11:08:20 +0300

    "9"

--- resources/views/admin/archive/index.blade.php
+++ resources/views/admin/archive/index.blade.php
@@ -1,39 +1,85 @@
 @extends('admin.layouts.main')
 
 @section('content')
-    <div class="card">
-        <!-- /.card-header -->
-        <div class="card-body col-4">
-            <table class="table table-bordered text-right">
-                <thead>
-                <tr>
-                    <th>Удобрения</th>
-                    @foreach($products as $product)
-                        <th><a href="{{ route('admin.archive.product', $product->id) }}">{{ $product->id }}</a></th>
-                    @endforeach
-                </tr>
-                <tr>
-                    <th>Клиенты</th>
-                    @foreach($clients as $client)
-                        <th><a href="{{ route('admin.archive.client', $client->id) }}">{{ $client->id }}</a></th>
-                    @endforeach
-                </tr>
-                <tr>
-                    <th>Культуры</th>
-                    @foreach($cultures as $culture)
-                        <th><a href="{{ route('admin.archive.culture', $culture->id) }}">{{ $culture->id }}</a></th>
-                    @endforeach
-                </tr>
-                <tr>
-                    <th>Пользователи</th>
-                    @foreach($users as $user)
-                        <th><a href="{{ route('admin.archive.user', $user->id) }}">{{ $user->id }}</a></th>
-                    @endforeach
-                </tr>
-                </thead>
 
+    <div class="row col-10">
+        <div>
+            <table width="100%" class="text-right position-absolute border-right-0">
+                <td>
+                    <table width="100%" cellpadding="2" cellspacing="1">
+                        <tr>
+                            <td bgcolor="#ffffff">Удобрения</td>
+                        </tr>
+                        <tr>
+                            @foreach($products as $product)
+                                <td><a href="{{ route('admin.archive.product', $product->id) }}">{{ $product->id }}</a>
+                                </td>
+                            @endforeach
+                        </tr>
+                        <tr>
+                            <td>
+                                {{ $products->links() }}
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+                <td>
+                    <table width="100%" cellpadding="2" cellspacing="1">
+                        <tr>
+                            <td bgcolor="#ffffff">Клиенты</td>
+                        </tr>
+                        <tr>
+                            @foreach($clients as $client)
+                                <td><a href="{{ route('admin.archive.client', $client->id) }}">{{ $client->id }}</a>
+                                </td>
+
+                            @endforeach
+                        </tr>
+                        <tr>
+                            <td>
+                                {{ $clients->links() }}
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+                <td>
+                    <table width="100%" cellpadding="2" cellspacing="1">
+                        <tr>
+                            <td bgcolor="#ffffff">Культуры</td>
+                        </tr>
+                        <tr>
+                            @foreach($cultures as $culture)
+                                <td><a href="{{ route('admin.archive.culture', $culture->id) }}">{{ $culture->id }}</a>
+                                </td>
+
+                            @endforeach
+                        </tr>
+                        <tr>
+                            <td bgcolor="#ffffff">
+                                {{ $cultures->links() }}
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+                <td>
+                    <table width="100%" cellpadding="2" cellspacing="1">
+                        <tr>
+                            <td bgcolor="#ffffff">Пользователи</td>
+                        </tr>
+                        <tr>
+                            @foreach($users as $user)
+                                <td><a href="{{ route('admin.archive.user', $user->id) }}">{{ $user->id }}</a></td>
+
+                            @endforeach
+                        </tr>
+                        <tr>
+                            <td bgcolor="#ffffff">
+                                {{ $users->links() }}
+                            </td>
+                        </tr>
+                    </table>
+                </td>
             </table>
         </div>
-
     </div>
 @endsection
